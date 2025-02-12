bc8b13b1d9b1b516c91555dde58853024bd7218c
rorxtto <pedrohenriqueroratto@gmail.com>
2025-02-12 20:48:30 -0300

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Sidebar from "./components/Sidebar";
 
 function App() {
 
@@ -9,16 +10,16 @@ function App() {
   return (
     <>
     <div className='tarefaPagina'>
-
+    <Sidebar />
       <div className='container'>
         <div className='containerTitulo'>
            <h1 className='titulo'>Minha Lista</h1>
            <button className='btn'>Nova</button>
         </div>
       </div>
-      
     </div>
     </>
+    
   )
 }
 
