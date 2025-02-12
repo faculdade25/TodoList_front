dc312d8aa2391ac693b1fa8f9dfebb7f2a0e6806
77Eduardo <103968776+77Eduardo@users.noreply.github.com>
2025-02-12 19:31:42 -0300

    projetoInicio

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className='tarefaPagina'>
+
+      <div className='container'>
+        <div className='containerTitulo'>
+           <h1 className='titulo'>Minha Lista</h1>
+           <button className='btn'>Nova</button>
+        </div>
+      </div>
+      
+    </div>
+    </>
+  )
+}
+
+export default App
