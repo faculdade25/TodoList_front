ec9ad17d6f3569587c8dedbaba1ff8b2cb22507f
rorxtto <pedrohenriqueroratto@gmail.com>
2025-02-13 21:41:02 -0300

    Sidebar + Tela Principal

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,18 +3,30 @@ import { FaList, FaPlus, FaSignOutAlt } from "react-icons/fa";
 import "./Sidebar.css";
 
 
-const MeuComponente = () => {
+const MeuComponente = ({ lists, onSelectList }) => {
+
+    
+
+  
   return (
     <div className="sidebar">
-    <h2  className="header">Olá! *Guest*</h2>
+    <h2  className="header-sidebar">Olá! *Guest*</h2>
     <nav>
-      <button className="list-item"><FaList /> Minha lista</button>
-      <button className="list-item"><FaList /> Minha listinha</button>
-      <button className="add-list"><FaPlus /> Criar lista</button>
-    </nav>
+        {lists.map((list, index) => (
+          <button key={index} onClick={() =>  onSelectList(index)} className="list-item">
+            <FaList /> {list.name}
+          </button>
+        ))}
+        <button className="add-list"><FaPlus /> Criar lista</button>
+      </nav>
     <button className="logout"><FaSignOutAlt /> Sair</button>
   </div>
   );
 };
+
+// MeuComponente.propTypes = {
+//   lists: PropTypes.array.isRequired,  // Espera um array de listas
+//   onSelectList: PropTypes.func.isRequired,  // Espera uma função
+// };
 
 export default MeuComponente;
